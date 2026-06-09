b3f220723b0e45e2ca774b90a0a8d3eab25203a9
lokmanekerboua <lokmankerboua@gmail.com>
2026-06-09 13:06:23 +0200

    some changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,6 +6,17 @@ allprojects {
 }
 
 subprojects {
+    project.evaluationDependsOn(":app")
+
+    // Force des versions stables pour éviter l'erreur "requires AGP 8.9.1"
+    configurations.all {
+        resolutionStrategy {
+            force("androidx.browser:browser:1.8.0")
+            force("androidx.core:core:1.13.1")
+            force("androidx.core:core-ktx:1.13.1")
+        }
+    }
+
     project.plugins.withId("com.android.application") {
         val android = project.extensions.findByName("android")
         if (android is com.android.build.gradle.BaseExtension) {
